010d1d324021b363e2f71aff4263a4bc5eecf7de
Eder manoel <edermartelinho@hotmail.com>
2022-04-07 09:20:25 -0300

    Atualizacoes

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,6 +1,32 @@
-const getAll = (req, res) => {
-    return res.render("index");
+const Task = require('../models/Task');
+
+
+const getAllTasks = async(req, res) => {
+    try {
+        const taskList = await Task.find();
+        return res.render("index", {
+            taskList
+        });
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
+};
+
+const createTask = async(req, res) => {
+    const task = req.body;
+
+    if (!task.task) {
+        return res.redirect("/")
+    }
+
+    try {
+        await Task.create(task)
+        return res.redirect("/")
+    } catch (err) {
+        res.status(500).send({ error: err.message });
+    }
 };
 module.exports = {
-    getAll,
+    getAllTasks,
+    createTask,
 }
