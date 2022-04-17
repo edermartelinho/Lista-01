037631d6bbc5a51d3b3a04598845102fe094d5c8
Eder manoel <edermartelinho@hotmail.com>
2022-04-17 09:23:15 -0300

    Ajustes,form,delete,modal

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
-const express = require('express');
+const express = require("express");
 const path = require("path");
 const routes = require("./routes/routes");
 const connectToDb = require("./database/db");
 
 connectToDb();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 app.use(routes);
